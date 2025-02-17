5b2bda165487058b583538dc0d735ebc4f560966
BugHunter-X <askzenai@gmail.com>
2025-02-17 21:14:05 +0100

    Add factorial_solution.py file

---
+++ factorial_solution.py
@@ -0,0 +1,21 @@
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+number = 5
+try:
+    result = factorial(number)
+    print(f"The factorial of {number} is {result}")
+except ValueError as e:
+    print(e) 
+
+number = -5
+try:
+    result = factorial(number)
+    print(f"The factorial of {number} is {result}")
+except ValueError as e:
+    print(e) 
